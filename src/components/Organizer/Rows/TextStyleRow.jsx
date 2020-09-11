1b57be01a94be6f057032bd297efd7f1cb666e38
RomainPct <romain.penchenat@gmail.com>
2020-09-11 22:22:49 +0200

    📠 Working textstyle row action

--- src/components/Organizer/Rows/TextStyleRow.jsx
+++ src/components/Organizer/Rows/TextStyleRow.jsx
@@ -1,12 +1,24 @@
 import { TextStyle } from '../../../utils/AssetModels'
 
 const React = require('react')
+const application = require('application')
 require('./Row.css')
 require('./TextStyleRow.css')
 
 export const TextStyleRow = ({ textStyle = new TextStyle("unknown", {})}) => {
+
+    const clickAction = () => {
+        application.editDocument(selection => {
+            selection.items.forEach(item => {
+                for (const [key, value] of Object.entries(textStyle.style)) {
+                    item[key] = value
+                }
+            })
+        })
+    }
+
     return (
-        <p className="row textStyleRow" style={{'borderColor' : textStyle.getHex()}}>
+        <p className="row textStyleRow" style={{'borderColor' : textStyle.getHex()}} onClick={clickAction}>
             {textStyle.name}
             <span className="details">{`${textStyle.style.fontSize}pt ${textStyle.style.fontStyle}`}</span>
         </p>
